46dfb7f078383b180ffee83e303c4b5709e116b5
Adela Tanca <adela.tanca@lateral-inc.com>
2021-12-18 22:30:49 +0200

    working aws auth

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,15 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = {
+  resolver: {
+    blacklistRE: exclusionList([/amplify\/#current-cloud-backend\/.*/]),
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
