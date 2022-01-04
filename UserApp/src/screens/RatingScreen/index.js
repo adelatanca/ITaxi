18ef0d25b40625e0adeb5c6aec2ed3e9deadb064
Adela Tanca <adela.tanca@lateral-inc.com>
2022-01-04 13:56:25 +0200

    added star rating half

--- UserApp/src/screens/RatingScreen/index.js
+++ UserApp/src/screens/RatingScreen/index.js
@@ -1,86 +1,130 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+// import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+
+// const RatingScreen = () => {
+//   const [defaultRating, setDefaultRating] = useState(2);
+//   const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+
+//   const starImgFilled =
+//     "https://github.com/tranhonghan/images/blob/main/star_filled.png?raw=true";
+//   const starImgCorner =
+//     "https://github.com/tranhonghan/images/blob/main/star_corner.png?raw=true";
+
+//   const CustomRatingBar = () => {
+//     return (
+//       <View style={styles.customRatingBar}>
+//         {maxRating.map((item, key) => {
+//           return (
+//             <TouchableOpacity
+//               activeOpacity={0.7}
+//               key={item}
+//               onPress={() => setDefaultRating(item)}
+//             >
+//               <Image
+//                 style={styles.starImgStyle}
+//                 source={
+//                   item <= defaultRating
+//                     ? { uri: starImgFilled }
+//                     : { uri: starImgCorner }
+//                 }
+//               />
+//             </TouchableOpacity>
+//           );
+//         })}
+//       </View>
+//     );
+//   };
+
+//   return (
+//     <View>
+//       <Text style={styles.textStyle}>Rate driver </Text>
+//       <CustomRatingBar />
+//       <Text style={styles.textStyle}>
+//         {defaultRating + "/" + maxRating.length}
+//       </Text>
+//       <TouchableOpacity
+//         activeOpacity={0.7}
+//         style={styles.buttonStyle}
+//         onPress={() => alert(defaultRating)}
+//       >
+//         <Text> Get Selected Rating </Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     margin: 10,
+//     justifyContent: "center",
+//   },
+//   textStyle: {
+//     textAlign: "center",
+//     fontSize: 23,
+//     marginTop: 20,
+//   },
+//   customRatingBar: {
+//     justifyContent: "center",
+//     flexDirection: "row",
+//     marginTop: 30,
+//   },
+//   starImgStyle: {
+//     width: 40,
+//     height: 40,
+//     resizeMode: "cover",
+//   },
+//   buttonStyle: {
+//     justifyContent: "center",
+//     alignItems: "center",
+//     marginTop: 30,
+//     padding: 15,
+//     margin: 10,
+//     backgroundColor: "pink",
+//   },
+// });
+
+// export default RatingScreen;
+
+import React, { useState, useEffect } from "react";
+import StarRating from "react-native-star-rating";
+
+import { View, StyleSheet, Text } from "react-native";
 
 const RatingScreen = () => {
-  const [defaultRating, setDefaultRating] = useState(2);
-  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+  const [rating, setRating] = useState(2.5);
 
-  const starImgFilled =
-    "https://github.com/tranhonghan/images/blob/main/star_filled.png?raw=true";
-  const starImgCorner =
-    "https://github.com/tranhonghan/images/blob/main/star_corner.png?raw=true";
-
-  const CustomRatingBar = () => {
-    return (
-      <View style={styles.customRatingBar}>
-        {maxRating.map((item, key) => {
-          return (
-            <TouchableOpacity
-              activeOpacity={0.7}
-              key={item}
-              onPress={() => setDefaultRating(item)}
-            >
-              <Image
-                style={styles.starImgStyle}
-                source={
-                  item <= defaultRating
-                    ? { uri: starImgFilled }
-                    : { uri: starImgCorner }
-                }
-              />
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    );
+  const onStarRatingPress = (rating) => {
+    setRating(rating);
   };
 
+  useEffect(() => {
+    console.log(rating);
+  }, [rating]);
+
   return (
-    <View>
-      <Text style={styles.textStyle}>Rate driver </Text>
-      <CustomRatingBar />
-      <Text style={styles.textStyle}>
-        {defaultRating + "/" + maxRating.length}
-      </Text>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        style={styles.buttonStyle}
-        onPress={() => alert(defaultRating)}
-      >
-        <Text> Get Selected Rating </Text>
-      </TouchableOpacity>
-    </View>
+    <Text style={styles.container}>
+      <StarRating
+        style={styles.textStyle}
+        halfStarEnabled={true}
+        maxStars={5}
+        rating={rating}
+        fullStarColor={"gold"}
+        selectedStar={(rating) => onStarRatingPress(rating)}
+      />
+    </Text>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 10,
+    margin: 100,
     justifyContent: "center",
   },
   textStyle: {
     textAlign: "center",
     fontSize: 23,
     marginTop: 20,
-  },
-  customRatingBar: {
-    justifyContent: "center",
-    flexDirection: "row",
-    marginTop: 30,
-  },
-  starImgStyle: {
-    width: 40,
-    height: 40,
-    resizeMode: "cover",
-  },
-  buttonStyle: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 30,
-    padding: 15,
-    margin: 10,
-    backgroundColor: "pink",
   },
 });
 
